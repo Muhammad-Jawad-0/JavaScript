fd93ee48bb2af84737e61d024fc4cec5b8b3bf24
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-02-15 19:25:13 +0500

    hugya

--- Js-Interveiw-question.js
+++ Js-Interveiw-question.js
@@ -5,19 +5,37 @@
 //                            // 2      0
 // let uniqueArr = arr.filter((element,index) => {
 //     ///////////////    2 = 0        0 // true
+//     // return arr.indexOf(element) === index
 //     return arr.indexOf(element) === index
 // })
 // //////////[2] <<<<<< ;
 // //////////[2,3,4,5,6,"banana",9];
 // console.log(uniqueArr)
 
-// ================= Fibonacci Series ==============
+// ==========================================
 
+// let arr = [2, 3, 4, 2, 4, 5, 6, 6, "banana", 9, 5, 3, "banana"];
+
+// let unique = arr.filter((element, index) => {
+//   return arr.findIndex((ele) => ele === element) === index;
+// });
+
+// console.log(unique);
+
+// let uniqueArr = arr.filter((element,index) => {
+
+//     return console.log(arr.findIndex((ele) => ele === element)=== index)
+// })
+
+// //////////[2,3,4,5,6,"banana",9];
+// console.log(uniqueArr)
+
+// ================= Fibonacci Series ==============
 
 // const fibonacci = (num) => {
 //     let arr = [0,1];
 
-//     for(let i = 2; i < num; i++){ 
+//     for(let i = 2; i < num; i++){
 //         arr.push(arr[i - 1] + arr[i - 2]) // indexes ke element ko add kra ha
 //     }
 
@@ -29,27 +47,21 @@
 
 // console.log(series) //[0,1,1,2,3,5,8];
 
-
 // ================= Reverse String using build In Method ==============
 
 // let string = "Muhammd Jawad";
 
 // let reverseString = string.split("").reverse().join("");
 
-
 // console.log(reverseString)
-
 
 // ================= Reverse String with Method ==============
 
 // let string = "Muhammd Jawad"; // l = 13 index = 12
 // let reverseStr = ""
 
-
 // for(let i = string.length - 1; i >= 0; i--){
 //     reverseStr += string[i]
 // }
-
-
 
 // console.log(reverseStr)
