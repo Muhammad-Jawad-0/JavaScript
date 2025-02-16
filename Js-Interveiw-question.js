953224d161780c4d2b97e64bebc5edceddb24a85
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-02-17 01:33:57 +0500

    add

--- Js-Interveiw-question.js
+++ Js-Interveiw-question.js
@@ -2,14 +2,13 @@
 
 // let arr = [2,3,4,2,4,5,6,6,"banana",9,5,3,"banana"];
 
-//                            // 2      0
+                           // 2      0
 // let uniqueArr = arr.filter((element,index) => {
-//     ///////////////    2 = 0        0 // true
-//     // return arr.indexOf(element) === index
-//     return arr.indexOf(element) === index
+    ///////////////    2 = 0        0 // true
+    // return arr.indexOf(element) === index
 // })
-// //////////[2] <<<<<< ;
-// //////////[2,3,4,5,6,"banana",9];
+//////////[2] <<<<<< ;
+//////////[2,3,4,5,6,"banana",9];
 // console.log(uniqueArr)
 
 // ==========================================
@@ -19,6 +18,15 @@
 // let unique = arr.filter((element, index) => {
 //   return arr.findIndex((ele) => ele === element) === index;
 // });
+
+// let result = arr.indexOf(2) // le loop nh chalata ha
+// console.log("result >>", result)
+
+// ===============================================
+
+// let res = arr.findIndex((ele,index) => console.log(ele)) // ye loop chalata ha..
+
+// console.log("res >>>", res)
 
 // console.log(unique);
 
@@ -65,3 +73,107 @@
 // }
 
 // console.log(reverseStr)
+
+
+
+// function foo(i) {
+//     if (i < 0) {
+//       return;
+//     }
+//     console.log(`begin: ${i}`);
+//     foo(i - 1);
+//     console.log(`end: ${i}`);
+//   }
+//   foo(3);
+
+
+
+
+
+
+
+// function counter() {
+//     let count = 0;
+  
+//     return function () {
+//       return ++count;
+//     };
+//   }
+
+//   console.log(counter()())
+//   let res = counter();
+
+//   console.log(res())
+
+
+
+
+// function multiply(a, b = 1) {
+//     return a * b;
+//   }
+  
+//   console.log(multiply(5, "8"));
+
+
+// function regular() {
+//     console.log(this)
+// }
+
+// regular()
+
+// const arrow = () => {
+//     console.log(this)
+// }
+// arrow()
+
+
+// const obj = {
+//     name: "Ali",
+//     showName: function () {
+//       const arrowFunc = () => {
+//         console.log(this.name); // ✅ `this` lexical scope se aayega
+//       };
+//       arrowFunc();
+//     }
+//   };
+
+
+
+// function name(params) {
+//     let merraNaam = "Jawad"
+
+//      const naam = () =>  this.merraNaam
+
+//      return naam
+// }
+
+// let a =  name()
+// console.log(a)
+
+
+// function Person() {
+//     this.age = 0;
+  
+//     setInterval(() => {
+//       console.log(this.age++); // `this` properly refers to the person object
+//     }, 1000);
+
+//   }
+  
+//   const p = new Person();
+
+
+
+// function Person() {
+//     // Some choose `that` instead of `self`.
+//     // Choose one and be consistent.
+//     const self = this;
+//     self.age = 0;
+  
+//     setInterval(function growUp() {
+//       // The callback refers to the `self` variable of which
+//       // the value is the expected object.
+//       console.log(self.age++);
+//     }, 1000);
+//   }
+// Person()
--- functions.js
+++ functions.js
@@ -69,3 +69,49 @@
 // const nehal = new Persion("nehal", 18);
 
 // console.log(nehal);
+
+
+// -----------------------closure Function --------------------
+
+// function addSquares(a, b) {
+//     function square(x) {
+//       return x * x;
+//     }
+//     return square(a) + square(b);
+//   }
+
+//   let res = addSquares(5,2);
+
+//   console.log(res);
+
+
+// -------------------------------------------------
+
+
+// const factorial = function(n) {
+//     return n < 1 ? 1 : n * factorial(n - 1);
+//   };
+  
+//   console.log(factorial(5)); // 120
+
+
+// -------------------------------------------------
+
+
+// function map(f, a) {
+//     // const result = new Array(a.length);
+//     const result = new Array(a.length);
+//     console.log(result);
+//     for (let i = 0; i < a.length; i++) {
+//       result[i] = f(a[i]);
+//     }
+//     return result;
+//   }
+  
+//   const numbers = [0, 1, 2, 5, 10];
+//   const cubedNumbers = map(function (x) {
+//     return x * x * x;
+//   }, numbers);
+//   console.log(cubedNumbers); // [0, 1, 8, 125, 1000]
+
+
